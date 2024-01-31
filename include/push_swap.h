5a87a2c77023ad702fc7c0966306fc84e2e7b381
Doooriian <dorian.ley91@gmail.com>
2024-01-31 15:12:36 +0100

    ajout de split.c

--- include/push_swap.h
+++ include/push_swap.h
@@ -6,11 +6,26 @@
 /*   By: dley <dley@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 11:56:58 by dley              #+#    #+#             */
-/*   Updated: 2024/01/23 12:00:56 by dley             ###   ########.fr       */
+/*   Updated: 2024/01/31 11:40:59 by dley             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
 # define PUSH_SWAP_H
+
+# include <stdbool.h>
+# include <stdlib.h>
+
+typedef struct s_stack_node
+{
+	int					nbr;
+	int					index;
+	int					push_price;
+	bool				above_median;
+	bool				cheapest;
+	struct s_stack_node	*target_node;
+	struct s_stack_node	*prev;
+	struct s_stack_node	*next;
+}				t_stack_node;
 
 #endif
--- src/push_swap.c
+++ src/push_swap.c
@@ -6,7 +6,7 @@
 /*   By: dley <dley@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/01/23 12:07:11 by dley              #+#    #+#             */
-/*   Updated: 2024/01/23 13:57:15 by dley             ###   ########.fr       */
+/*   Updated: 2024/01/31 11:43:05 by dley             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,9 +14,14 @@
 
 int	main(int argc, char **argv)
 {
-	if (argc < 2 || !(argv[1][0]))
-	{	
-		return (0);
-	}
+	t_stack_node	*stack_a;
+	t_stack_node	*stack_b;
+
+	stack_a = NULL;
+	stack_b = NULL;
+	if (argc == 1 || (argc == 2 && !argv[1][0]))
+		return (1);
+	else if (argc == 2)
+		argv = ft_split(argv[1], ' ');
 	return (0);
 }
