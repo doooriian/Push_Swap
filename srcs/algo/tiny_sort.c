da938904573c658ebb3f69cdf8d53b6f6120f242
Doooriian <dorian.ley91@gmail.com>
2024-02-11 18:15:31 +0100

    add tiny_sort.c

--- srcs/algo/tiny_sort.c
+++ srcs/algo/tiny_sort.c
@@ -6,8 +6,31 @@
 /*   By: dley <dley@student.42.fr>                  +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/11 14:35:49 by dley              #+#    #+#             */
-/*   Updated: 2024/02/11 17:56:34 by dley             ###   ########.fr       */
+/*   Updated: 2024/02/11 18:15:03 by dley             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "../../include/push_swap.h"
+
+void	sort_three(t_stack_node **a)
+{
+	t_stack_node	*max;
+
+	max = find_max(*a);
+	if (*a == max)
+		ra(a, false);
+	else if ((*a)->next == max)
+		rra(a, false);
+	if ((*a)->nbr > (*a)->next->nbr)
+		sa(a, false);
+}
+
+void	sort_five(t_stack_node **a, t_stack_node **b)
+{
+	while (stack_len(*a) > 3)
+	{
+		init_nodes_a(*a, *b);
+		prep_for_push(a, find_min(*a), 'a');
+		pb(b, a, false);
+	}
+}
